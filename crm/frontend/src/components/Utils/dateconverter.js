aa70592042dc47fe7c346586c9cd20f3da27ffc0
Rahul P R <56236803+Chipsetjunkie@users.noreply.github.com>
2020-10-30 13:31:15 +0530

    Update dateconverter.js

--- crm/frontend/src/components/Utils/dateconverter.js
+++ crm/frontend/src/components/Utils/dateconverter.js
@@ -24,23 +24,8 @@ export const displayTimeLine = data =>{
   const date = new Date(data)
   const  d = new Date()
 
-  if(d > date){
-    return `${date.getDate()} ${ months[date.getMonth()]}, ${date.getFullYear()}`
-  }
 
-  if (date.getFullYear() > d.getFullYear()){
-    return `${months[date.getMonth()]}, ${date.getFullYear()}`
-  }
-  if (date.getMonth() < d.getMonth()){
-    return `${date.getDate()}, ${months[date.getMonth()]}`
-  }
+  return `${date.getDate()} ${ months[date.getMonth()]}, ${date.getFullYear()}`
 
-  if (date.getDate() - d.getDate()>1 ){
-    return` ${date.getDate()} ${days[date.getDay()]}`
-  }
-
-  else{
-    return `${date.getHours()}: ${date.getMinutes()}`
-  }
 
 }
